b35d6a530d2d300525c43db51ce9286314ef551a
pratk <pratk1812@gmail.com>
2025-05-25 18:27:06 +0530

    chat integrated

---
+++ src/main/java/com/ragnarson/StudentMVC/persistence/converter/LocalDateTimeConvertor.java
@@ -0,0 +1,26 @@
+package com.finan.myaichat.persistence.convertor;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Converter(autoApply = true)
+public class LocalDateConvertor implements AttributeConverter<LocalDateTime, Timestamp> {
+
+  @Override
+  public Timestamp convertToDatabaseColumn(LocalDateTime localDateTime) {
+    if (localDateTime != null) {
+      return Timestamp.valueOf(localDateTime);
+    }
+    return null;
+  }
+
+  @Override
+  public LocalDateTime convertToEntityAttribute(Timestamp timestamp) {
+    if (timestamp != null) {
+      return timestamp.toLocalDateTime();
+    }
+    return null;
+  }
+}
